3759181a3b28fcbdc8b1d48363aee3bcde45bcf7
Pos3ydon <tobias.leitner2002@gmail.com>
2021-11-29 16:20:29 +0100

    some fixes

--- Website/scripts/appendField.js
+++ Website/scripts/appendField.js
@@ -10,14 +10,11 @@ function closeAppendFieldWindow() {
 function appendField(formId, buttonId) {
     document.getElementById(formId).setAttribute("style", "display: block");
     document.getElementById(buttonId).setAttribute("style", "display: none");
-
-    closeAppendFieldWindow();
 }
 
 function deleteField(formId, buttonId) {
-    /*alert(document.getElementById(formId).children.item(1).getAttribute("value"));
-    document.getElementById(formId).getElementsByTagName("input")[0].setAttribute("value", "");*/
+    document.getElementById(formId).getElementsByTagName("input")[0].value = "";
+    document.getElementById(formId).getElementsByTagName("input")[0].oninput();
     document.getElementById(formId).setAttribute("style", "display: none");
     document.getElementById(buttonId).setAttribute("style", "display: block");
-    //alert(document.getElementById(formId).children.item(1).getAttribute("value"));
 }
