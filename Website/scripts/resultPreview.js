f87d23d7a54d90b36e08a8fac7f1c0f451f943e0
Tobias Leitner <tobias.leitner2002@gmail.com>
2021-11-29 19:25:43 +0100

    Different Changes
    Background-Changer,
    some Buttons improved,
    new Font-Family

--- Website/scripts/resultPreview.js
+++ Website/scripts/resultPreview.js
@@ -1,3 +1,24 @@
+$(document).ready(function(){
+    $("#previewChangeBackgroundButton").bind({
+        mouseenter: function(e) {
+            document.getElementById("changeBackgroundWindow").setAttribute("style", "display: block");
+        },
+        mouseleave: function(e){
+            document.getElementById("changeBackgroundWindow").setAttribute("style", "display: none");
+        }
+    });
+    
+    $("#changeBackgroundWindow").bind({
+        mouseenter: function(e) {
+            document.getElementById("changeBackgroundWindow").setAttribute("style", "display: block");
+        },
+        mouseleave: function(e){
+            document.getElementById("changeBackgroundWindow").setAttribute("style", "display: none");
+        }
+    });
+});
+
+
 function openLogoImage(event) {
     var selected = event.target.files[0];
     var fr = new FileReader();
@@ -9,4 +30,20 @@ function openLogoImage(event) {
 
 function updateValue(sender, output) {
     document.getElementById(output).textContent = event.target.value;
+}
+
+function changeBackgroundColor(event) {
+    document.getElementById("preview").style.backgroundColor = event.target.value;
+    document.getElementById("preview").style.backgroundImage = null;
+}
+
+function openBackgroundImage(event) {
+    var selected = event.target.files[0];
+    var fr = new FileReader();
+    fr.onload = function (event) {
+        document.getElementById("preview").style.backgroundImage = "url('" + event.target.result + "')";
+    }
+    fr.readAsDataURL(event.target.files[0]);
+    
+    document.getElementById("preview").style.backgroundColor = null;
 }
