d21dbe0da7a41b28ba2deb12acd778826200d102
Tobias Leitner <tobias.leitner2002@gmail.com>
2021-11-30 19:21:18 +0100

    Draggable Preview

--- Website/scripts/appendField.js
+++ Website/scripts/appendField.js
@@ -1,20 +1,39 @@
+$(document).ready(function() {
+    $("#new_textfield").bind({
+        click: function(e) {
+            document.getElementById("appendFieldWindowDiv").style.display = "block";
+        }
+    });
+    $("#btn_closeAppendFieldWindow").bind({
+        click: function(e) {
+            document.getElementById("appendFieldWindowDiv").style.display = "none";
+        }
+    });
+});
 
-function openAppendFieldWindow() {
+
+/*function openAppendFieldWindow() {
     document.getElementById("appendFieldWindowDiv").setAttribute("style", "display: block");
-}
+}*/
 
-function closeAppendFieldWindow() {
+/*function closeAppendFieldWindow() {
     document.getElementById("appendFieldWindowDiv").setAttribute("style", "display: none");
-}
+}*/
 
 function appendField(formId, buttonId) {
-    document.getElementById(formId).setAttribute("style", "display: block");
-    document.getElementById(buttonId).setAttribute("style", "display: none");
+    document.getElementById(formId).style.display = "block";
+    document.getElementById(buttonId).style.display = "none";
+    //document.getElementById(formId).setAttribute("style", "display: block");
+    //document.getElementById(buttonId).setAttribute("style", "display: none");
 }
 
 function deleteField(formId, buttonId) {
-    document.getElementById(formId).getElementsByTagName("input")[0].value = "";
+    //document.getElementById(formId).getElementsByTagName("input")[0].value = "";
+    document.getElementById(formId).getElementsByTagName("input")[0].oninput();
+    document.getElementById(formId).style.display = "none";
+    document.getElementById(buttonId).style.display = "block";
+    /*document.getElementById(formId).getElementsByTagName("input")[0].value = "";
     document.getElementById(formId).getElementsByTagName("input")[0].oninput();
     document.getElementById(formId).setAttribute("style", "display: none");
-    document.getElementById(buttonId).setAttribute("style", "display: block");
+    document.getElementById(buttonId).setAttribute("style", "display: block");*/
 }
--- Website/scripts/resultPreview.js
+++ Website/scripts/resultPreview.js
@@ -1,55 +1,89 @@
-$(document).ready(function(){
-    $("#previewChangeBackgroundButton").bind({
+$(document).ready(function() {
+    $(".previewField").draggable();
+    
+    $("#previewChangeBackgroundBtn").bind({
         mouseenter: function(e) {
-            document.getElementById("changeBackgroundWindow").setAttribute("style", "display: block");
+            document.getElementById("changeBackgroundWindow").style.display = "block";
+            //document.getElementById("changeBackgroundWindow").setAttribute("style", "display: block");
         },
-        mouseleave: function(e){
-            document.getElementById("changeBackgroundWindow").setAttribute("style", "display: none");
+        mouseleave: function(e) {
+            document.getElementById("changeBackgroundWindow").style.display = "none";
+            //document.getElementById("changeBackgroundWindow").setAttribute("style", "display: none");
         }
     });
     
     $("#changeBackgroundWindow").bind({
         mouseenter: function(e) {
-            document.getElementById("changeBackgroundWindow").setAttribute("style", "display: block");
+            document.getElementById("changeBackgroundWindow").style.display = "block";
+            //document.getElementById("changeBackgroundWindow").setAttribute("style", "display: block");
         },
-        mouseleave: function(e){
-            document.getElementById("changeBackgroundWindow").setAttribute("style", "display: none");
+        mouseleave: function(e) {
+            document.getElementById("changeBackgroundWindow").style.display = "none";
+            //document.getElementById("changeBackgroundWindow").setAttribute("style", "display: none");
+        }
+    });
+    
+    $("#changeBackgroundColorBtn").bind({
+        input: function(e) {
+            document.getElementById("preview").style.backgroundColor = event.target.value;
+            document.getElementById("preview").style.backgroundImage = null;
         }
     });
     
     $("#openBackgroundImageBtn").bind({
-       click: function(e){
-           document.getElementById("openBackgroundImageBtn").value = null;
-       } 
+        click: function(e) {
+            document.getElementById("openBackgroundImageBtn").value = null;
+        },
+        input: function(e) {
+            var selected = e.target.files[0];
+            var fr = new FileReader();
+            fr.onload = function(e) {
+                document.getElementById("preview").style.backgroundImage = "url('" + e.target.result + "')";
+            }
+            fr.readAsDataURL(e.target.files[0]);
+
+            document.getElementById("preview").style.backgroundColor = null;
+        }
+    });
+    
+    $("#openPreviewLogoBtn").bind({
+        input: function(e) {
+            var selected = e.target.files[0];
+            var fr = new FileReader();
+            fr.onload = function(e) {
+                document.getElementById("previewLogo").style.backgroundImage = "url('" + e.target.result + "')";
+            }
+            fr.readAsDataURL(e.target.files[0]);
+        }
     });
 });
 
 
-function openLogoImage(event) {
+/*function openBackgroundImage(event) {
     var selected = event.target.files[0];
     var fr = new FileReader();
-    fr.onload = function (event) {
-        document.getElementById("previewLogo").style.backgroundImage = "url('" + event.target.result + "')";
-    }
-    fr.readAsDataURL(event.target.files[0]);
-}
-
-function updateValue(sender, output) {
-    document.getElementById(output).textContent = event.target.value;
-}
-
-function changeBackgroundColor(event) {
-    document.getElementById("preview").style.backgroundColor = event.target.value;
-    document.getElementById("preview").style.backgroundImage = null;
-}
-
-function openBackgroundImage(event) {
-    var selected = event.target.files[0];
-    var fr = new FileReader();
-    fr.onload = function (event) {
+    fr.onload = function(event) {
         document.getElementById("preview").style.backgroundImage = "url('" + event.target.result + "')";
     }
     fr.readAsDataURL(event.target.files[0]);
     
     document.getElementById("preview").style.backgroundColor = null;
+}*/
+
+/*function changeBackgroundColor(event) {
+    document.getElementById("preview").style.backgroundColor = event.target.value;
+    document.getElementById("preview").style.backgroundImage = null;
+}*/
+
+/*function openLogoImage(event) {
+    var selected = event.target.files[0];
+    var fr = new FileReader();
+    fr.onload = function(event) {
+        document.getElementById("previewLogo").style.backgroundImage = "url('" + event.target.result + "')";
+    }
+    fr.readAsDataURL(event.target.files[0]);
+}*/
+
+function updateValue(output) {
+    document.getElementById(output).textContent = event.target.value;
 }
